8c185f23672c21585822834f7cfe0e41b8428e26
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-08-26 12:33:52 -0700

    changed structure and started questions

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+inquirer
+  .prompt([{
+    type: 'input',
+    name: 'Project Title',
+    message: 'What is the title of your Project?',
+    default: 'My Github Project'
+    },
+    {
+      type: 'input',
+      name: 'Project Description',
+      message: 'What is the description of your project?',
+      default: 'My project does some pretty cool stuff, as described'
+      },
+    // /* Pass your questions in here */
+    // questions
+    ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+    console.log(answers)
+  });
+
+// TODO: Create an array of questions for user input
+// const questions = [
+    
+// ];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
